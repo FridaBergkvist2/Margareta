5258e99ce6ad555b407d666892efc9d0d53586c4
FridaBergkvist2 <fridaber10@student.astar.se>
2025-08-11 14:56:24 +0200

    Uppdatera filerna

--- Margareta/Program.cs
+++ Margareta/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main ()
+	{
+		List<Produkt> produkter = new List <Produkt>();
+
+		produkter.Add (new Produkt ("Grönsakstårtan",456));
+
+		bool kör = true;
+
+		while (kör)
+		{
+			try
+			{
+				Console.Write ("Ange en tårta, eller skriv 'stopp'");
+				string namn = Console.ReadLine();
+
+				if(namn.ToLower () =="stopp")
+				{
+					kör = false;
+					continue;
+				}
+
+				Console.Write ("Kostnad för tårtan:");
+				double pris = Convert.ToDouble(Console.ReadLine());
+
+				produkter.Add (new Produkt (namn,pris));
+			}
+
+			catch
+			{
+				Console.WriteLine ("Du har inte skrivit ett tal");
+			}
+		}
+
+		Console.WriteLine ("\nTårtlistan");
+		foreach (Produkt p in produkter)
+		{
+			p.VisaInfo();
+		}
+	}
+}
